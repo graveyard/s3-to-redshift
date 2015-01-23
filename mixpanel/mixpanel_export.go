d8a74f3a7025a235767ef01ae5c9757f7f1b60b3
Abhinav Garg <abhinav.garg@clever.com>
2015-01-23 12:08:39 -0800

    Move mixpanel export functions to a separate library.

---
+++ mixpanel/mixpanel_export.go
@@ -0,0 +1,104 @@
+package mixpanel
+
+import (
+	"crypto/md5"
+	"encoding/hex"
+	"encoding/json"
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"net/url"
+	"sort"
+	"time"
+)
+
+var (
+	//TODO: Use flag validators
+	apikey    = flag.String("mixpanelapikey", "", "API Key for Mixpanel.")
+	apisecret = flag.String("mixpanelapisecret", "", "API Secret for Mixpanel")
+	timeout   = flag.Duration("mixpanelexporttimeout", 10*time.Minute,
+		"Timeout value to be used while making a mixpanel export request. Defaults to 10 mins.")
+)
+
+type MixpanelExport struct {
+	endpoint   string
+	version    string
+	apiKey     string
+	apiSecret  string
+	httpGetter func(string) (*http.Response, error)
+	nowGetter  func() time.Time
+}
+
+func NewMixpanelExport() *MixpanelExport {
+	m := MixpanelExport{"https://data.mixpanel.com/api", "2.0", *apikey, *apisecret, http.Get, time.Now}
+	return &m
+}
+
+// json.Marshal wraps a string with quotes which breaks URL params in API calls. This function fixes
+// that problem.
+func StringOrJsonMarshal(value interface{}) (string, error) {
+	strValue, ok := value.(string)
+	if !ok {
+		jsonValue, err := json.Marshal(value)
+		if err != nil {
+			return "", err
+		}
+		strValue = string(jsonValue)
+	}
+	return strValue, nil
+}
+
+func (m *MixpanelExport) ComputeSig(params map[string]interface{}) (string, error) {
+	var keys []string
+
+	for k := range params {
+		keys = append(keys, k)
+	}
+	sort.Strings(keys)
+
+	joinedArgs := ""
+	for _, k := range keys {
+		joinedArgs += k
+		value, err := StringOrJsonMarshal(params[k])
+		if err != nil {
+			return "", err
+		}
+		joinedArgs += fmt.Sprintf("=%s", value)
+	}
+
+	hash := md5.Sum([]byte(joinedArgs + m.apiSecret))
+	return hex.EncodeToString(hash[:]), nil
+}
+
+func (m *MixpanelExport) Request(method string, params map[string]interface{}) ([]byte, error) {
+	params["api_key"] = m.apiKey
+	expireTime := m.nowGetter().Add(*timeout)
+	params["expire"] = expireTime.Unix()
+	params["format"] = "json"
+
+	delete(params, "sig")
+	sig, err := m.ComputeSig(params)
+	if err != nil {
+		return nil, err
+	}
+	params["sig"] = sig
+
+	values := url.Values{}
+	for key, value := range params {
+		strValue, err := StringOrJsonMarshal(value)
+		if err != nil {
+			return nil, err
+		}
+		values.Set(key, strValue)
+	}
+	url := fmt.Sprintf("%s/%s/%s/?%s", m.endpoint, m.version, method, values.Encode())
+	log.Println(url)
+	resp, err := m.httpGetter(url)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+	return ioutil.ReadAll(resp.Body)
+}
---
+++ mixpanel/mixpanel_export_test.go
@@ -0,0 +1,65 @@
+package mixpanel
+
+import (
+	"bytes"
+	"io/ioutil"
+	"net/http"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+)
+
+type marshalTestPair struct {
+	value    interface{}
+	strvalue string
+}
+
+func TestStringOrJsonMarshal(t *testing.T) {
+	tests := []marshalTestPair{
+		{"teststring", "teststring"},
+		{map[string]string{"key1": "val1", "key2:": "val2"}, "{\"key1\":\"val1\",\"key2:\":\"val2\"}"},
+		{[]string{"arrary", "of", "strings"}, "[\"arrary\",\"of\",\"strings\"]"},
+	}
+	for _, pair := range tests {
+		v, err := StringOrJsonMarshal(pair.value)
+		assert.NoError(t, err)
+		assert.Equal(t, pair.strvalue, v)
+	}
+}
+
+func TestComputeSig(t *testing.T) {
+	params := map[string]interface{}{
+		"api_key": "TESTMIXPANELAPIKEY",
+		"expire":  12312324324,
+		"events":  []string{"event1", "event2"},
+		"param1":  "value1",
+	}
+	m, exp := MixpanelExport{apiSecret: "TESTMIXPANELAPISECRET"}, "1beb8d4f61da24302844e252a8ff6e75"
+	sig, err := m.ComputeSig(params)
+	assert.NoError(t, err)
+	assert.Equal(t, exp, sig)
+}
+
+func mockHttpGet(url string) (*http.Response, error) {
+	reader := bytes.NewReader([]byte(url))
+	mockbody := ioutil.NopCloser(reader)
+	return &http.Response{Body: mockbody}, nil
+}
+
+func mockTimeGetter() time.Time {
+	return time.Unix(10000000, 0)
+}
+
+func TestRequest(t *testing.T) {
+	m := MixpanelExport{"https://data.mixpanel.com/api", "2.0", "apikey", "apisecret", mockHttpGet, mockTimeGetter}
+	params := map[string]interface{}{
+		"events": []string{"event1", "event2"},
+		"param1": "value1",
+		"param2": "value2",
+	}
+	expurl := "https://data.mixpanel.com/api/2.0/method/?api_key=apikey&events=%5B%22event1%22%2C%22event2%22%5D&expire=10000600&format=json&param1=value1&param2=value2&sig=825125f9640ce6bee7b6c25fd33eacb3"
+	url, err := m.Request("method", params)
+	assert.NoError(t, err)
+	assert.Equal(t, expurl, url)
+}
