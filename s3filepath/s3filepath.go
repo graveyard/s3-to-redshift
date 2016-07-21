3fbbe5a519f3ed9b8cbe01e38d18168a406b7af3
Colin Schimmelfing <colin.schimmelfing@clever.com>
2016-07-21 15:15:21 -0700

    refactor the file discovery code slightly

--- s3filepath/s3filepath.go
+++ s3filepath/s3filepath.go
@@ -69,21 +69,16 @@ func CreateS3File(pc PathChecker, bucket S3Bucket, schema, table, suppliedConf s
 		confFile = suppliedConf
 	}
 
-	// Try to find manifest or data files in the order of:
-	// 1) manifest file
-	// 2) gzipped json file
-	// 3) json file
-	inputFile := S3File{bucket, schema, table, "auto", "manifest", date, confFile}
-	if pc.FileExists(inputFile.GetDataFilename()) {
-		return &inputFile, nil
-	}
-	inputFile = S3File{bucket, schema, table, "auto", "json.gz", date, confFile}
-	if pc.FileExists(inputFile.GetDataFilename()) {
-		return &inputFile, nil
-	}
-	inputFile = S3File{bucket, schema, table, "auto", "json", date, confFile}
-	if pc.FileExists(inputFile.GetDataFilename()) {
-		return &inputFile, nil
+	// Try to find manifest or data files out of the following patterns, in order
+	// we try to get in order as otherwise
+	for _, suffix := range []string{
+		"manifest", // 1) manifest file
+		"json.gz",  // 2) gzipped json file
+		"json"} {   // 3) json file
+		inputFile := S3File{bucket, schema, table, "auto", suffix, date, confFile}
+		if pc.FileExists(inputFile.GetDataFilename()) {
+			return &inputFile, nil
+		}
 	}
 	return nil, fmt.Errorf("S3 file not found at: bucket: %s schema: %s, table: %s date: %s",
 		bucket.Name, schema, table, formattedDate)
